eea6d72d97236e4274d2d16b0c4c0926d7440235
Anand Kumar <anand.parihat@gmail.com>
2025-11-11 23:51:57 +0530

    Delete backend directory

--- backend/models/Issue.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const IssueSchema = new mongoose.Schema({
-  description: String,
-  location: { type: [Number], index: '2dsphere' }, // [lat, lng]
-  severity: String,
-  status: String,
-  reportedAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Issue', IssueSchema);
--- backend/models/Notification.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const NotificationSchema = new mongoose.Schema({
-  message: String,
-  createdAt: { type: Date, default: Date.now },
-  read: { type: Boolean, default: false }
-});
-module.exports = mongoose.model('Notification', NotificationSchema);
